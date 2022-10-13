6a82e66524683a2c537350c0470e2aa71a1d87e3
suizenji <suizenji@pop-os.localdomain>
2022-10-13 13:02:48 +0900

    data fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Comment;
+use App\Entity\Conference;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $conference = new Conference();
+        $conference->setCity('Washinton');
+        $conference->setYear('2020');
+        $conference->setIsInternational(false);
+
+        $manager->persist($conference);
+        $manager->flush();
+
+        $comment = new Comment();
+        $comment->setAuthor('Kyosuke');
+        $comment->setText('hello');
+        $comment->setEmail('kyosuke@google.com');
+        $comment->setCreatedAt(new \DateTimeImmutable());
+        $comment->setConference($conference);
+
+        $manager->merge($comment);
+        $manager->flush();
+    }
+}
