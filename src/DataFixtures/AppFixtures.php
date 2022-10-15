d08193788890e42e8eefbb97b79bbcae7c062b6a
suizenji <suizenji@pop-os.localdomain>
2022-10-15 12:36:41 +0900

    data

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -30,5 +30,38 @@ class AppFixtures extends Fixture
 #        $manager->merge($comment);
         $manager->persist($comment);
         $manager->flush();
+
+        $this->storeNewYork2021($manager);
+    }
+
+    private function storeNewYork2021($manager)
+    {
+        $conference = new Conference();
+        $conference->setCity('NewYork');
+        $conference->setYear('2021');
+        $conference->setIsInternational(true);
+
+        $manager->persist($conference);
+        $manager->flush();
+
+        $comment = new Comment();
+        $comment->setAuthor('Jhon');
+        $comment->setText('lorem ipsum');
+        $comment->setEmail('jhon@google.com');
+        $comment->setCreatedAt(new \DateTimeImmutable());
+        $conference->addComment($comment);
+
+        $manager->persist($comment);
+        $manager->flush();
+
+        $comment = new Comment();
+        $comment->setAuthor('Smith');
+        $comment->setText('foobarctjm3q94ugecgjhuher;ahmcgaowcerchma8wethgae;cngyaw8ot;fqacw48ntchertusewhnctawoinwfueichngawoiuc;gocn;aocmy awerctg[wna;ohgwcnhawcngcwnichcwcfghnuanhgciocnerionccawhcgfhacwhgiocna8ncgw8cngaw8chng8wnacuincucga8wn8gor');
+        $comment->setEmail('smith@google.com');
+        $comment->setCreatedAt(new \DateTimeImmutable());
+        $conference->addComment($comment);
+
+        $manager->persist($comment);
+        $manager->flush();
     }
 }
