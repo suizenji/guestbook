00b5be2343efc46c9c15b71151d5e42a9d80b05f
suizenji <suizenji@pop-os.localdomain>
2022-10-27 12:31:04 +0900

    migration

--- migrations/Version20221024042349.php
+++ migrations/Version20221024042349.php
@@ -26,7 +26,7 @@ final class Version20221024042349 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
+#        $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP INDEX UNIQ_911533C8989D9B62');
     }
 }
