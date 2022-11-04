68188eaeb603a850b8292411e3f057b8b6b6703d
suizenji <suizenji@pop-os.localdomain>
2022-11-04 13:03:39 +0900

    fix-test

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -2,8 +2,8 @@
 
 namespace App\Tests\Controller;
 
-// use App\Repository\CommentRepository;
-// use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\CommentRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ConferenceControllerTest extends WebTestCase
@@ -12,17 +12,15 @@ class ConferenceControllerTest extends WebTestCase
     {
         $client = static::createClient();
         $client->request('GET', '/');
-        // $client->request('GET', '/en/');
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Give your feedback');
     }
 
-    /*
     public function testCommentSubmission()
     {
         $client = static::createClient();
-        $client->request('GET', '/en/conference/amsterdam-2019');
+        $client->request('GET', '/conference/amsterdam-2019');
         $client->submitForm('Submit', [
             'comment_form[author]' => 'Fabien',
             'comment_form[text]' => 'Some feedback from an automated functional test',
@@ -33,34 +31,25 @@ class ConferenceControllerTest extends WebTestCase
 
         // simulate comment validation
         $comment = self::getContainer()->get(CommentRepository::class)->findOneByEmail($email);
-        $comment->setState('published');
+        //        $comment->setState('published');
         self::getContainer()->get(EntityManagerInterface::class)->flush();
 
         $client->followRedirect();
-        $this->assertSelectorExists('div:contains("There are 2 comments")');
+        $this->assertSelectorExists('div:contains("There are 3 comments")');
     }
-    */
 
     public function testConferencePage()
     {
-        /** \Symfony\Bundle\FrameworkBundle\KernelBrowser $client */
         $client = static::createClient();
-
-        /** \Symfony\Component\DomCrawler\Crawler $crawler */
         $crawler = $client->request('GET', '/');
 
         $this->assertCount(2, $crawler->filter('h4'));
 
         $client->clickLink('View');
 
-#        $response = $client->getResponse();
-#        var_dump($response->getContent());
-#        file_put_contents('index.html', $response->getContent());
-#        return;
-
         $this->assertPageTitleContains('Amsterdam');
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Amsterdam 2019');
-        $this->assertSelectorExists('div:contains("There are 2 comment")');
+        $this->assertSelectorExists('div:contains("There are 2 comments")');
     }
 }
